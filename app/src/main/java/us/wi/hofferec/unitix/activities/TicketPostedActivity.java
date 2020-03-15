274cbddc3ff16d29f5f396dd13392339da51fbc2
John <jbednarczyk@wisc.edu>
2020-03-15 15:08:27 -0500

    Improved TicketMarketplace and structure

--- app/src/main/java/us/wi/hofferec/unitix/activities/TicketPostedActivity.java
+++ app/src/main/java/us/wi/hofferec/unitix/activities/TicketPostedActivity.java
@@ -15,26 +15,10 @@ import us.wi.hofferec.unitix.R;
 
 public class TicketPostedActivity extends AppCompatActivity {
 
-    private RecyclerView marketRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_posted);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        marketRecyclerView.setHasFixedSize(true);
-
-        // Use a linear layout manager
-        layoutManager = new LinearLayoutManager(this);
-        marketRecyclerView.setLayoutManager(layoutManager);
-
-        // specify an adapter TODO Replace null with datasource
-        mAdapter = new MyAdapter(null);
-        marketRecyclerView.setAdapter(mAdapter);
     }
 
     public void goToHome(View view){
@@ -47,53 +31,5 @@ public class TicketPostedActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
         startActivity(intent);
         finish();
-    }
-
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-        private String[] mDataset;
-
-        // Provide a reference to the views for each data item
-        // Complex data items may need more than one view per item, and
-        // you provide access to all the views for a data item in a view holder
-        public class MyViewHolder extends RecyclerView.ViewHolder {
-            // each data item is just a string in this case
-            public TextView textView;
-            public MyViewHolder(TextView v) {
-                super(v);
-                textView = v;
-            }
-        }
-
-        // Provide a suitable constructor (depends on the kind of dataset)
-        public MyAdapter(String[] myDataset) {
-            mDataset = myDataset;
-        }
-
-        // Create new views (invoked by the layout manager)
-        @Override
-        public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            // create a new view
-            TextView textView = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_ticket_marketplace, parent, false);
-
-            //...
-
-            MyViewHolder viewHolder = new MyViewHolder(textView);
-            return viewHolder;
-        }
-
-        // Replace the contents of a view (invoked by the layout manager)
-        @Override
-        public void onBindViewHolder(MyViewHolder holder, int position) {
-            // - get element from your dataset at this position
-            // - replace the contents of the view with that element
-            holder.textView.setText(mDataset[position]);
-
-        }
-
-        // Return the size of your dataset (invoked by the layout manager)
-        @Override
-        public int getItemCount() {
-            return mDataset.length;
-        }
     }
 }
