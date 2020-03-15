ecdd0d177988de1f62418219c5d360524eca5fbd
John <jbednarczyk@wisc.edu>
2020-03-14 19:21:34 -0500

    Functional Settings Screen

--- app/src/main/java/us/wi/hofferec/unitix/SettingsActivity.java
+++ app/src/main/java/us/wi/hofferec/unitix/SettingsActivity.java
@@ -1,24 +1,36 @@
 package us.wi.hofferec.unitix;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    public static final String NOTIFICATION_SWITCH = "notificationsKey";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // Start the settings fragment
+        SettingsFragment settingsFragment = new SettingsFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.activity_settings, settingsFragment)
+                .commit();
     }
 
-    public void goToProfile(View view) {
-        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
-        finish();
     }
 }
---
+++ app/src/main/java/us/wi/hofferec/unitix/SettingsFragment.java
@@ -0,0 +1,52 @@
+package us.wi.hofferec.unitix;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    SharedPreferences sharedPreferences;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        // Get sharedPreferences
+        if (getActivity() != null)
+            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case "notificationsKey":
+                boolean on = sharedPreferences.getBoolean("notificationsKey", false);
+                if (on) {
+                    Toast.makeText(getActivity(), "Notifications enabled", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getActivity(), "Notifications disabled", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+}
