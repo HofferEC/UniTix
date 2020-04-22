de9cc19d295359af35dd6567bbca9c54b7680599
timschabel <tschabel@wi.rr.com>
2020-04-21 22:45:31 -0500

    Added Google Pay support for buying tickets

--- app/src/main/java/us/wi/hofferec/unitix/activities/ConfirmPurchaseActivity.java
+++ app/src/main/java/us/wi/hofferec/unitix/activities/ConfirmPurchaseActivity.java
@@ -1,36 +1,274 @@
 package us.wi.hofferec.unitix.activities;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentData;
+import com.google.android.gms.wallet.PaymentDataRequest;
+import com.google.android.gms.wallet.PaymentsClient;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Optional;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import us.wi.hofferec.unitix.R;
 import us.wi.hofferec.unitix.data.Ticket;
+import us.wi.hofferec.unitix.databinding.ConfirmPurchaseBinding;
+import us.wi.hofferec.unitix.helpers.PaymentsUtil;
 
 public class ConfirmPurchaseActivity extends AppCompatActivity {
 
     Ticket ticket;
 
+    // Arbitrarily-picked constant integer you define to track a request for payment data activity.
+    private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 991;
+
+    // A client for interacting with the Google Pay API.
+    private PaymentsClient paymentsClient;
+
+    private ConfirmPurchaseBinding layoutBinding;
+    private View googlePayButton;
+
+    private JSONArray garmentList;
+    private JSONObject selectedGarment;
+
+    /**
+     * Initialize the Google Pay API on creation of the activity
+     *
+     * @see Activity#onCreate(android.os.Bundle)
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.confirm_purchase);
 
         Intent intent = getIntent();
         ticket = (Ticket) intent.getSerializableExtra("ticket");
 
+        // Use view binding to access the UI elements
+        layoutBinding = ConfirmPurchaseBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.confirm_purchase);
+
         TextView ticketInfo = (TextView) findViewById(R.id.ticketInfoTextView);
 
         ticketInfo.setText("Event: " + ticket.getEvent() + "\n\n" +
-                    "Teams: " + ticket.getAwayTeam() + " @ " + ticket.getHomeTeam() + "\n\n" +
-                    "Date: " + ticket.getDate() + "\n\n" +
-                    "Price: $" + ticket.getPrice());
+                "Teams: " + ticket.getAwayTeam() + " @ " + ticket.getHomeTeam() + "\n\n" +
+                "Date: " + ticket.getDate() + "\n\n" +
+                "Price: $" + ticket.getPrice());
+
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //Notifications.createNotificationChannelIfNotCreated(this);
+        }
+
+        // Initialize a Google Pay API client for an environment suitable for testing.
+        // It's recommended to create the PaymentsClient object inside of the onCreate method.
+        paymentsClient = PaymentsUtil.createPaymentsClient(this);
+        possiblyShowGooglePayButton();
+    }
+
+    /**
+     * Handle a resolved activity from the Google Pay payment sheet.
+     *
+     * @param requestCode Request code originally supplied to AutoResolveHelper in requestPayment().
+     * @param resultCode  Result code returned by the Google Pay API.
+     * @param data        Intent from the Google Pay API containing payment or error data.
+     * @see <a href="https://developer.android.com/training/basics/intents/result">Getting a result
+     * from an Activity</a>
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            // value passed in AutoResolveHelper
+            case LOAD_PAYMENT_DATA_REQUEST_CODE:
+                switch (resultCode) {
+
+                    case Activity.RESULT_OK:
+                        PaymentData paymentData = PaymentData.getFromIntent(data);
+                        handlePaymentSuccess(paymentData);
+                        break;
+
+                    case Activity.RESULT_CANCELED:
+                        // The user cancelled the payment attempt
+                        break;
+
+                    case AutoResolveHelper.RESULT_ERROR:
+                        Status status = AutoResolveHelper.getStatusFromIntent(data);
+                        handleError(status.getStatusCode());
+                        break;
+                }
+        }
+    }
+
+    /**
+     * Determine the viewer's ability to pay with a payment method supported by your app and display a
+     * Google Pay payment button.
+     *
+     * @see <a href="https://developers.google.com/android/reference/com/google/android/gms/wallet/
+     * PaymentsClient.html#isReadyToPay(com.google.android.gms.wallet.
+     * IsReadyToPayRequest)">PaymentsClient#IsReadyToPay</a>
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void possiblyShowGooglePayButton() {
+
+        final Optional<JSONObject> isReadyToPayJson = PaymentsUtil.getIsReadyToPayRequest();
+        if (!isReadyToPayJson.isPresent()) {
+            return;
+        }
+
+        // The call to isReadyToPay is asynchronous and returns a Task. We need to provide an
+        // OnCompleteListener to be triggered when the result of the call is known.
+        IsReadyToPayRequest request = IsReadyToPayRequest.fromJson(isReadyToPayJson.get().toString());
+        Task<Boolean> task = paymentsClient.isReadyToPay(request);
+        task.addOnCompleteListener(this,
+                new OnCompleteListener<Boolean>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        if (task.isSuccessful()) {
+                            setGooglePayAvailable(task.getResult());
+                        } else {
+                            Log.w("isReadyToPay failed", task.getException());
+                        }
+                    }
+                });
+    }
+
+    /**
+     * If isReadyToPay returned {@code true}, show the button and hide the "checking" text. Otherwise,
+     * notify the user that Google Pay is not available. Please adjust to fit in with your current
+     * user flow. You are not required to explicitly let the user know if isReadyToPay returns {@code
+     * false}.
+     *
+     * @param available isReadyToPay API response.
+     */
+    private void setGooglePayAvailable(boolean available) {
+        if (available) {
+            //googlePayButton.setVisibility(View.VISIBLE);
+        } else {
+            Toast.makeText(this, "Google Pay Unavailable", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * PaymentData response object contains the payment information, as well as any additional
+     * requested information, such as billing and shipping address.
+     *
+     * @param paymentData A response object returned by Google after a payer approves payment.
+     * @see <a href="https://developers.google.com/pay/api/android/reference/
+     * object#PaymentData">PaymentData</a>
+     */
+    private void handlePaymentSuccess(PaymentData paymentData) {
+
+        // Token will be null if PaymentDataRequest was not constructed using fromJson(String).
+        final String paymentInfo = paymentData.toJson();
+        if (paymentInfo == null) {
+            return;
+        }
+
+        try {
+            JSONObject paymentMethodData = new JSONObject(paymentInfo).getJSONObject("paymentMethodData");
+            // If the gateway is set to "example", no payment information is returned - instead, the
+            // token will only consist of "examplePaymentMethodToken".
+
+            final JSONObject tokenizationData = paymentMethodData.getJSONObject("tokenizationData");
+            final String tokenizationType = tokenizationData.getString("type");
+            final String token = tokenizationData.getString("token");
+
+            if ("PAYMENT_GATEWAY".equals(tokenizationType) && "examplePaymentMethodToken".equals(token)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Warning")
+                        .setMessage("This is a test message")
+                        .setPositiveButton("OK", null)
+                        .create()
+                        .show();
+            }
+
+            final JSONObject info = paymentMethodData.getJSONObject("info");
+            final String billingName = info.getJSONObject("billingAddress").getString("name");
+            Toast.makeText(
+                    this, "Test transaction complete!",
+                    Toast.LENGTH_LONG).show();
+
+            // Logging token string.
+            Log.d("Google Pay token: ", token);
+
+            Intent intent = new Intent(getApplicationContext(), TicketPurchasedActivity.class);
+            intent.putExtra("ticket", ticket);
+            startActivity(intent);
+
+        } catch (JSONException e) {
+            throw new RuntimeException("The selected garment cannot be parsed from the list of elements");
+        }
+    }
+
+    /**
+     * At this stage, the user has already seen a popup informing them an error occurred. Normally,
+     * only logging is required.
+     *
+     * @param statusCode will hold the value of any constant from CommonStatusCode or one of the
+     *                   WalletConstants.ERROR_CODE_* constants.
+     * @see <a href="https://developers.google.com/android/reference/com/google/android/gms/wallet/
+     * WalletConstants#constant-summary">Wallet Constants Library</a>
+     */
+    private void handleError(int statusCode) {
+        Log.w("loadPaymentData failed", String.format("Error code: %d", statusCode));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void requestPayment(View view) {
+
+        // Disables the button to prevent multiple clicks.
+        //googlePayButton.setClickable(false);
+
+        // The price provided to the API should include taxes and shipping.
+        // This price is not displayed to the user.
+        long ticketPriceCents = Math.round(Double.parseDouble(ticket.getPrice()) * PaymentsUtil.CENTS_IN_A_UNIT.longValue());
+
+        Optional<JSONObject> paymentDataRequestJson = PaymentsUtil.getPaymentDataRequest(ticketPriceCents);
+        if (!paymentDataRequestJson.isPresent()) {
+            return;
+        }
+
+        PaymentDataRequest request =
+                PaymentDataRequest.fromJson(paymentDataRequestJson.get().toString());
+
+        // Since loadPaymentData may show the UI asking the user to select a payment method, we use
+        // AutoResolveHelper to wait for the user interacting with it. Once completed,
+        // onActivityResult will be called with the result.
+        if (request != null) {
+            AutoResolveHelper.resolveTask(
+                    paymentsClient.loadPaymentData(request),
+                    this, LOAD_PAYMENT_DATA_REQUEST_CODE);
+        }
+
+
     }
 
     public void goToHome(View view){
@@ -41,11 +279,5 @@ public class ConfirmPurchaseActivity extends AppCompatActivity {
 
     public void cancel(View view){
         finish();
-    }
-
-    public void buyTicket(View view){
-        Intent intent = new Intent(getApplicationContext(), TicketPurchasedActivity.class);
-        intent.putExtra("ticket", ticket);
-        startActivity(intent);
     }
 }
