2fcb965755e6a310bdf435635aa1ef65775a2442
mdespe <mikespe23@gmail.com>
2020-04-23 14:09:14 -0500

    fixed bug in ProfileSellingActivity that crashed app if user has no tickets bought

--- app/src/main/java/us/wi/hofferec/unitix/activities/ProfileSellingActivity.java
+++ app/src/main/java/us/wi/hofferec/unitix/activities/ProfileSellingActivity.java
@@ -75,9 +75,11 @@ public class ProfileSellingActivity extends AppCompatActivity {
                         if (document.exists()) {
                             List<DocumentReference> list = (List<DocumentReference>) document.get("ticketsSelling");
                             List<Task<DocumentSnapshot>> tasks = new ArrayList<>();
-                            for (DocumentReference documentReference : list) {
-                                Task<DocumentSnapshot> documentSnapshotTask = documentReference.get();
-                                tasks.add(documentSnapshotTask);
+                            if (list != null) {
+                                for (DocumentReference documentReference : list) {
+                                    Task<DocumentSnapshot> documentSnapshotTask = documentReference.get();
+                                    tasks.add(documentSnapshotTask);
+                                }
                             }
                             // Only move to the next step when all data is loaded
                             Tasks.whenAllSuccess(tasks).addOnSuccessListener(new OnSuccessListener<List<Object>>() {
