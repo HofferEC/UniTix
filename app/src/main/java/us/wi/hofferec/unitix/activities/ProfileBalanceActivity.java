5632b6ac42ce8d4fada0eed81bb6993fa2389cc0
John Bednarczyk <johnbednarczykjr@gmail.com>
2020-04-30 22:43:14 -0500

    Fixed changing currency on balance screen

--- app/src/main/java/us/wi/hofferec/unitix/activities/ProfileBalanceActivity.java
+++ app/src/main/java/us/wi/hofferec/unitix/activities/ProfileBalanceActivity.java
@@ -52,11 +52,31 @@ public class ProfileBalanceActivity extends AppCompatActivity {
         Utility.updateUser("ProfileBalanceActivity");
         String balance;
         if (LoginActivity.user.getBalance() < 0.0) {
+
             balance = String.format("%.2f", -LoginActivity.user.getBalance());
-            balanceTV.setText("-$" + balance);
+
+            if (LoginActivity.user.getSettings().get("currency").equals("USD")){
+                balanceTV.setText("-$" + balance);
+            }
+            else if (LoginActivity.user.getSettings().get("currency").equals("EUR")) {
+                balanceTV.setText("-€" + Utility.convert(Double.parseDouble(balance), "EUR"));
+            }
+            else if (LoginActivity.user.getSettings().get("currency").equals("GBP")) {
+                balanceTV.setText("-£" + Utility.convert(Double.parseDouble(balance), "GBP"));
+            }
+
         } else {
             balance = String.format("%.2f", LoginActivity.user.getBalance());
-            balanceTV.setText("$" + balance);
+
+            if (LoginActivity.user.getSettings().get("currency").equals("USD")){
+                balanceTV.setText("$" + balance);
+            }
+            else if (LoginActivity.user.getSettings().get("currency").equals("EUR")) {
+                balanceTV.setText("€" + Utility.convert(Double.parseDouble(balance), "EUR"));
+            }
+            else if (LoginActivity.user.getSettings().get("currency").equals("GBP")) {
+                balanceTV.setText("£" + Utility.convert(Double.parseDouble(balance), "GBP"));
+            }
         }
 
         checkPerms("ProfileBuyingActivity");
